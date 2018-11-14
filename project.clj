8ababde30f9b7f8e64d1a5909ea2478dc807c0ff
Gianluca Scacco <gianluca.scacco@gmail.com>
2018-11-14 15:17:01 +0100

    Updated with the forked versions

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject gorilla-plot "0.1.4"
+(defproject org.clojars.gscacco/gorilla-plot "0.5.0"
   :description "A simple data-driven plotting library for Gorilla REPL."
-  :url "https://github.com/JonyEpsilon/gorilla-plot"
+  :url "https://github.com/gscacco/gorilla-plot.git"
   :license {:name "MIT"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [gorilla-renderable "2.0.0"]])
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [org.clojars.gscacco/gorilla-renderable "0.5.0"]])
